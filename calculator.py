a7762e0b39be4d83683f3b6c7f03b5f703d9706f
Rocky <rockys3933@gmail.com>
2022-12-08 15:09:12 +0530

    forth commit

--- calculator.py
+++ calculator.py
@@ -22,34 +22,33 @@ print("3.Multiply")
 print("4.Divide")
 #print("5.exit")
 
-while True:
+#while True:
     
     #choice = input("Enter choice(1/2/3/4):")
-    choice = 1
+    #choice = 1
 
    
-    if choice in ('1', '2', '3', '4',):
+    #if choice in ('1', '2', '3', '4',):
         #num1 = float(input("Enter first number: "))
         #num2 = float(input("Enter second number: "))
-        num1 = 2
-        num2 = 3
+num1 = 2
+num2 = 3
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-            choice = 2
-            break
+      
+print(num1, "+", num2, "=", add(num1, num2))
+    
+        
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-            choice = 3
+      
+print(num1, "-", num2, "=", subtract(num1, num2))
+            
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-            choice = 4
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-            choice=0
-            break
+        
+print(num1, "*", num2, "=", multiply(num1, num2))
+        
+        
+print(num1, "/", num2, "=", divide(num1, num2))
+        
             
         # next_calculation = input("Let's do next calculation? (yes/no): ")
         # if next_calculation == "no":
